58d4c7e3826edb76dd4752156f64d2ac9af99e72
Honglin Chen <94886510+darcy521@users.noreply.github.com>
2023-02-18 11:22:05 -0800

    configure edge runtime

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    runtime: "experimental-edge",
+  },
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com'
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,17 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
+import type { NextRequest } from "next/server";
 
-type Data = {
-  name: string
-}
+export const config = {
+  runtime: "experimental-edge",
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(
+  req: NextRequest
+): Promise<Response> {
+  return new Response(JSON.stringify({ name: "John Doe" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
