7d67a7635829f119aeae6f4dafd9e661ed93bf81
Yulin <37796757+meredith-zeng@users.noreply.github.com>
2023-02-18 22:55:13 -0800

    homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import {Button, Card, CardGroup, Col, Container, Row} from 'react-bootstrap'
+import {Button, Card, CardGroup, Col, Container, Row, Image} from 'react-bootstrap'
 import ExpendableText from "@/components/shared/ExpendableText";
 import React from "react";
 
@@ -19,6 +18,21 @@ const Home: NextPage = () => {
       </Head>
             <CardGroup className={styles.homePage}>
                 <div>
+
+
+                <CardGroup className={styles.startImg}>
+                    <Card>
+                            <Image
+                            src={
+                                'https://firebasestorage.googleapis.com/v0/b/female-period-support.appspot.com/o/Complete.jpg?alt=media&token=b3faa65e-7fe0-441d-bc1d-2a9db783a312'
+                            }
+                            alt="starter"
+                            width={'100%'}
+                            height={900}/>
+                    </Card>
+                </CardGroup>
+                </div>
+                <div>
                     <CardGroup className={styles.ourMission}>
                         <Card className={styles.txtGroup}>
                             <Card.Title className={styles.missionTitle}>Our Mission</Card.Title>
@@ -27,10 +41,15 @@ const Home: NextPage = () => {
                     </CardGroup>
                 </div>
                 <div className="container">
-                    <div style={{ paddingTop: '30px'}}>
+                    <CardGroup className={styles.getInvolved}>
+                        <h1>Get Involved</h1>
+                    </CardGroup>
+                    <div>
+
                         <div style={{ display: "flex" }} >
-                            <div className ="col-5">
-                                <Card style={{ width: '25rem' }}>
+
+                            <div className ="col-7 align-content-center">
+                                <Card style={{ width: '35rem' }}>
                                     <Card.Img className = "card_image"
                                               variant="top"
                                               src="https://firebasestorage.googleapis.com/v0/b/female-period-support.appspot.com/o/HelpingWomenPeriod_Logo.png?alt=media&token=65ae556b-eeee-45fa-9b28-98c0a6c6d764"/>
@@ -48,11 +67,8 @@ const Home: NextPage = () => {
                                     </Card.Body>
                                 </Card>
                             </div>
-                            <div className="col-3">
-                                <h1>Take Action!</h1>
-                            </div>
-                            <div className ="col-5">
-                                <Card style={{ width: '25rem' }}>
+                            <div className ="col-7 align-content-center">
+                                <Card style={{ width: '35rem' }}>
                                     <Card.Img className = "card_image"
                                               variant="top"
                                               src="https://firebasestorage.googleapis.com/v0/b/female-period-support.appspot.com/o/PeriodProject_Logo.png?alt=media&token=193717d8-900a-4d92-8057-321a7fc99bfb" />
