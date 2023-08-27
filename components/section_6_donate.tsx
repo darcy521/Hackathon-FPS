3a4e166ca5479281a06a3db3ac5e0579d67540f5
Lai <57856015+kingofbug@users.noreply.github.com>
2023-08-26 21:39:38 -0700

    Update section_6_donate.tsx

--- components/section_6_donate.tsx
+++ components/section_6_donate.tsx
@@ -11,9 +11,9 @@ import styles from '@/styles/Home.module.css';
 function Section_6_Donate(){
     return(
         <div style={{minHeight: '100vh'}}>
-            <h1 style={{textAlign: 'center',marginTop:'15%',lineHeight:'1.5',fontFamily:'brandon-grotesque'}}>What you can do besides in-person donation...</h1>
+            <h1 style={{textAlign: 'center',marginTop:'15%',lineHeight:'1.5',fontFamily:'brandon-grotesque',fontSize:'2.5em',paddingBottom:'5vh'}}>What you can do besides in-person donation <span style={{fontSize:'0.6em',color:'#D58989'}}>&#9660;</span></h1>
             <div style={{ display: 'flex',justifyContent:'center',alignItems:'center'}}>
-                <div style={{width:'80%',textAlign:'center',fontSize:'1.4em',fontFamily:'brandon-grotesque',lineHeight:'1.6'}}>
+                <div style={{width:'80%',textAlign:'center',fontSize:'1.5em',fontFamily:'brandon-grotesque',lineHeight:'1.6',paddingBottom:'5vh'}}>
                         By embracing the power of online giving, you&apos;re not only transforming the lives of homeless women but also fostering a culture of compassion and inclusivity. Your donation ripples outward, inspiring
                         others to join this noble cause and make a collective impact.
                 </div>
@@ -21,43 +21,35 @@ function Section_6_Donate(){
             <MDBCardGroup style={{padding: '2%'}}>
                 <MDBCard className='shadow-0'>
                     <div style={{width:'100%', height:'max-content', display: 'flex', alignItems:'center', justifyContent:'center', paddingTop:'4%'}}>
-                        <MDBRow >
-                            <MDBCol>
-                                <MDBCardImage style={{borderRadius: '50%', height: '120px', width:'120px'}}
-                                              src='https://mdbootstrap.com/img/new/standard/city/041.webp' alt='...' position='top'
+                                <MDBCardImage style={{ height: '120px', width:'auto'}}
+                                              src="https://firebasestorage.googleapis.com/v0/b/female-period-support.appspot.com/o/HelpingWomenPeriod_Logo.png?alt=media&token=65ae556b-eeee-45fa-9b28-98c0a6c6d764" alt='...' position='top'
                                 />
-
-                            </MDBCol>
-                            <MDBCol>
-                                <MDBCardImage style={{borderRadius: '50%', height: '120px', width:'120px'}}
-                                              src='https://mdbootstrap.com/img/new/standard/city/041.webp' alt='...' position='top'
-                                />
-                            </MDBCol>
-                        </MDBRow>
                     </div>
-
                 </MDBCard>
                 <MDBCard className='shadow-0'>
                     <div style={{width:'100%', height:'max-content', display: 'flex', alignItems:'center', justifyContent:'center', paddingTop:'4%'}}>
-                        <MDBRow >
-                            <MDBCol>
-
-                                <MDBCardImage style={{borderRadius: '50%', height: '120px', width:'120px'}}
-                                              src='https://mdbootstrap.com/img/new/standard/city/041.webp' alt='...' position='top'
+                        <MDBCardImage style={{ height: '120px', width:'auto'}}
+                                      src="https://firebasestorage.googleapis.com/v0/b/female-period-support.appspot.com/o/PeriodProject_Logo.png?alt=media&token=193717d8-900a-4d92-8057-321a7fc99bfb" alt='...' position='top'
+                        />
+                    </div>
+                </MDBCard>
+                <MDBCard className='shadow-0'>
+                    <div style={{width:'100%', height:'max-content', display: 'flex', alignItems:'center', justifyContent:'center', paddingTop:'4%'}}>
+                                <MDBCardImage style={{ height: '120px', width:'auto'}}
+                                              src="https://firebasestorage.googleapis.com/v0/b/female-period-support.appspot.com/o/Period_Logo.png?alt=media&token=975718c7-67db-478b-a4ac-6ba49700e458" alt='...' position='top'
                                 />
-
-                            </MDBCol>
-                            <MDBCol>
-                                <MDBCardImage style={{borderRadius: '50%', height: '120px', width:'120px'}}
-                                              src='https://mdbootstrap.com/img/new/standard/city/041.webp' alt='...' position='top'
-                                />
-                            </MDBCol>
-                        </MDBRow>
+                    </div>
+                </MDBCard>
+                <MDBCard className='shadow-0'>
+                    <div style={{width:'100%', height:'max-content', display: 'flex', alignItems:'center', justifyContent:'center', paddingTop:'4%'}}>
+                        <MDBCardImage style={{ height: '120px', width:'auto'}}
+                                      src="https://firebasestorage.googleapis.com/v0/b/female-period-support.appspot.com/o/PeriodPoveryuProject_Logo.png?alt=media&token=388191c9-985d-46f2-8d6a-72e6bf5b2115" alt='...' position='top'
+                        />
                     </div>
                 </MDBCard>
             </MDBCardGroup>
 
-            <div style={{display:'flex',alignItems:'center',justifyContent:'center',paddingTop:'5%'}}>
+            <div style={{display:'flex',alignItems:'center',justifyContent:'center',paddingTop:'5vh', paddingBottom:'5vh'}}>
                 <Button  href = "/donate" role='button' style={{background:'#fff',boxShadow:'none'}} className={styles.button86}>learn more</Button>
             </div>
 
